892be50067a1cb0fca6c682e62a96f13821bdf02
AzizOuertani <aziz@bearstudio.fr>
2024-03-11 15:06:02 +0100

    fix(Images):remove skeleton

--- src/components/ui/detailCard/DetailCard.tsx
+++ src/components/ui/detailCard/DetailCard.tsx
@@ -23,7 +23,6 @@ const DetailCard: React.FC<{ DetailCardData: DetailCardProps }> = ({
   return (
     <li className="relative z-50 overflow-hidden rounded-2xl  shadow-2xl duration-300   hover:translate-y-1 hover:shadow-lg">
       <NextImage
-        useSkeleton
         src={DetailCardData.imageUrl}
         fill
         priority={false}
--- src/features/header/_partial/Hero.tsx
+++ src/features/header/_partial/Hero.tsx
@@ -6,7 +6,6 @@ export default function Hero() {
     <div className="relative overflow-hidden pb-52">
       <div className="absolute left-0 top-0 z-10 h-full w-full">
         <NextImage
-          useSkeleton
           src="/images/hero-background.webp"
           fill
           style={{ objectFit: "cover" }}
