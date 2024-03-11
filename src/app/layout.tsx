4cd7925dc3bf31b67045d9c29598f95320f1c974
AzizOuertani <aziz@bearstudio.fr>
2024-03-11 10:20:15 +0100

    fix(fonts) remove inter fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,10 @@
-import { Metadata } from 'next';
-import { Inter as FontSans } from 'next/font/google';
-import * as React from 'react';
+import { Metadata } from "next";
+import * as React from "react";
 
-import '@/styles/globals.css';
+import "@/styles/globals.css";
 
-import { siteConfig } from '@/constant/config';
+import { siteConfig } from "@/constant/config";
 
-// !STARTERCONF Change these default meta
-// !STARTERCONF Look at @/constant/config to change them
 export const metadata: Metadata = {
   metadataBase: new URL(siteConfig.url),
   title: {
@@ -17,9 +14,9 @@ export const metadata: Metadata = {
   description: siteConfig.description,
   robots: { index: true, follow: true },
   icons: {
-    icon: '/favicon/favicon.ico',
-    shortcut: '/favicon/favicon-16x16.png',
-    apple: '/favicon/apple-touch-icon.png',
+    icon: "/favicon/favicon.ico",
+    shortcut: "/favicon/favicon-16x16.png",
+    apple: "/favicon/apple-touch-icon.png",
   },
   manifest: `/favicon/site.webmanifest`,
   openGraph: {
@@ -27,23 +24,17 @@ export const metadata: Metadata = {
     title: siteConfig.title,
     description: siteConfig.description,
     siteName: siteConfig.title,
-    // !STARTERCONF We need to update this image with an image for social media
     images: [`${siteConfig.url}/images/social_media_visual.png`],
-    type: 'website',
-    locale: 'fr_FR',
+    type: "website",
+    locale: "fr_FR",
   },
   twitter: {
-    card: 'summary_large_image',
+    card: "summary_large_image",
     title: siteConfig.title,
     description: siteConfig.description,
-    // !STARTERCONF We need to update this image with an image for social media
     images: [`${siteConfig.url}/images/social_media_visual.png`],
   },
 };
-const fontSans = FontSans({
-  subsets: ['latin'],
-  variable: '--font-sans',
-});
 
 export default function RootLayout({
   children,
@@ -51,7 +42,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang='fr' className={`${fontSans.variable}`}>
+    <html lang="fr">
       <body>{children}</body>
     </html>
   );
