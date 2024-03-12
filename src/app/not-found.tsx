ffbc3b89450a7f303eaf3e2b8f1494683ca5a9ad
AzizOuertani <aziz@bearstudio.fr>
2024-03-12 14:05:34 +0100

    fixer error page 404 not found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,27 +1,34 @@
-import { ChevronLeft, Siren } from 'lucide-react';
-import { Metadata } from 'next';
-import Link from 'next/link';
-import * as React from 'react';
+import { ChevronLeft, Siren } from "lucide-react";
+import { Metadata } from "next";
+import Link from "next/link";
+import * as React from "react";
 
-import { buttonVariants } from '@/components/ui/button';
+import { buttonVariants } from "@/components/ui/button";
 
 export const metadata: Metadata = {
-  title: 'Not Found',
+  title: "Not Found",
 };
 
 export default function NotFound() {
   return (
     <main>
-      <section className='bg-white'>
-        <div className='layout flex min-h-screen flex-col items-center justify-center text-center text-black'>
+      <section className="bg-white">
+        <div className="layout flex min-h-screen flex-col items-center justify-center text-center text-black">
           <Siren
             size={60}
-            className='drop-shadow-glow animate-flicker text-primary'
+            className="drop-shadow-glow animate-flicker text-gray-500"
           />
-          <h1 className='my-8 text-4xl md:text-6xl'>Page Not Found</h1>
+          <h1 className="mb-4 mt-8 text-5xl font-black md:text-6xl ">404</h1>
+
+          <h1 className="mb-8 text-5xl font-black md:text-6xl ">
+            Page Not Found
+          </h1>
           <Link
-            href='/'
-            className={`${buttonVariants({ size: 'lg' })}  px-2 text-xl`}
+            href="/"
+            className={`${buttonVariants({
+              variant: "outline",
+              size: "lg",
+            })} text-balck  px-2 text-xl `}
           >
             <ChevronLeft /> Back to home
           </Link>
